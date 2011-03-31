50e1c88aa46e03d2d8a1f69222d0042839a00f22
Nathaniel Bibler <git@nathanielbibler.com>
2011-03-31 13:21:43 -0400

    Correctly load the railtie

--- lib/sha_header.rb
+++ lib/sha_header.rb
@@ -1,3 +1,5 @@
+require 'sha_header/railtie' if defined?(::Rails::Railtie)
+
 module SHAHeader
   autoload 'Middleware', 'sha_header/middleware'
 end
--- lib/sha_header/railtie.rb
+++ lib/sha_header/railtie.rb
@@ -3,8 +3,8 @@ require 'rails'
 
 module SHAHeader
   class Railtie < Rails::Railtie
-    initializer "sha_header.configure_rails_initialization" do |app|
-      app.middleware.use SHAHeader::Middleware
+    initializer "sha_header.use_rack_middleware" do |app|
+      app.middleware.use "SHAHeader::Middleware"
     end
   end
 end
