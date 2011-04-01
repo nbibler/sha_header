02e706805c041f982b0f4c38b1381faa458e66b4
Nathaniel Bibler <nbibler@nathanielbibler.com>
2011-03-31 19:18:14 -0700

    Strip all inputs

--- lib/sha_header/middleware.rb
+++ lib/sha_header/middleware.rb
@@ -29,9 +29,9 @@ module SHAHeader
 
     def current_git_sha
       if revision_present?
-        File.read(::Rails.root.join('REVISION'))
+        File.read(::Rails.root.join('REVISION')).strip
       elsif on_heroku?
-        ENV['COMMIT_HASH']
+        ENV['COMMIT_HASH'].strip
       else
         `cd "#{::Rails.root}" && git rev-parse HEAD 2>/dev/null`.strip
       end
