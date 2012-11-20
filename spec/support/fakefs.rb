ac7f1876904a6d9a1e5b7e52e51f89459b5be84f
Nathaniel Bibler <git@nathanielbibler.com>
2012-11-20 08:51:36 -0500

    Reorganize and update support dependencies

--- spec/support/fakefs.rb
+++ spec/support/fakefs.rb
@@ -1,18 +1,5 @@
-require 'fakefs'
-
-module FakeFS::SpecHelpers
-  def use_fakefs
-    before(:each) do
-      FakeFS.activate!
-    end
-
-    after(:each) do
-      FakeFS.deactivate!
-      FakeFS::FileSystem.clear
-    end
-  end
-end
+require 'fakefs/spec_helpers'
 
 RSpec.configure do |config|
-  config.extend FakeFS::SpecHelpers
+  config.include FakeFS::SpecHelpers, :fakefs
 end
